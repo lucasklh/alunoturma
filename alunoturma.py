d74adc37324b71abce55a51be2f99d0765c25007
bathwaterpizza <cakto002@gmail.com>
2024-06-12 19:52:35 -0300

    Consertando formatação e namespaces de is_aprovado

--- alunoturma.py
+++ alunoturma.py
@@ -1,4 +1,6 @@
 import os, json, atexit, datetime
+from .. import curso
+from .. import cursoturma, avaliacaocurso, alunoavaliacao
 
 # Exportando funções de acesso
 __all__ = ["add_matricula", "del_matricula", "get_turmas_by_aluno", "get_alunos_by_turma", 
@@ -118,18 +120,18 @@ def is_aprovado(id_turma: int, id_aluno: int) -> tuple[int,bool]:
     Caso o aluno esteja aprovado, retornara True, caso não, False.
     """
 
-    #verifica as faltas do aluno no sistema
-    faltas= get_faltas(id_turma,id_aluno)
+    # verifica as faltas do aluno no sistema
+    faltas = get_faltas(id_turma,id_aluno)
     if get_faltas[0] != 0:
         return 27, False
 
     #encontra de que curso é a turma que este aluno se encontra
-    turma_curso = get_curso_by_turma(id_turma)
+    turma_curso = cursoturma.get_curso_by_turma(id_turma)
     if turma_curso[0] != 0:
         return 6, False
         
     #pega as informações necessárias para descobrir se o aluno está aprovado a partir do curso feito
-    curso= get_curso(turma_curso[1])
+    curso = curso.get_curso(turma_curso[1])
     if curso[0] != 0:
         return 27, True
 
@@ -139,16 +141,16 @@ def is_aprovado(id_turma: int, id_aluno: int) -> tuple[int,bool]:
         return 0, False
         
     #obtem os id's de provas aplicadas para esse curso nas turmas
-    avaliacoes = get_criterio(curso[1]["id"])
+    avaliacoes = avaliacaocurso.get_criterio(curso[1]["id"])
     if avaliacoes[0] != 0:
         return 26, False
 
     #armazena todas as notas de tiradas pelo aluno neste curso
-    notas_recebidas= []
+    notas_recebidas = []
 
     #percorre cada id e busca a nota encontrada pelo aluno na avaliação
     for i in range(len(avaliacoes[1])):
-        resposta_aluno = get_resposta(id_aluno,avaliacoes[1][i])
+        resposta_aluno = alunoavaliacao.get_resposta(id_aluno,avaliacoes[1][i])
         if resposta_aluno[0] != 0:
             return 13, False
         notas_recebidas.append(resposta_aluno[1]["nota"])
@@ -156,16 +158,11 @@ def is_aprovado(id_turma: int, id_aluno: int) -> tuple[int,bool]:
     #faz o somatório dos valores da lista e divide pelo número de 
     #avaliações que constam ter sido aplicadas para este curso
     
-    media= sum(notas_recebidas)/len(avaliacoes[1])
+    media = sum(notas_recebidas)/len(avaliacoes[1])
 
     #se a media for maior que 7, o aluno está aprovado
     if media >= 7:
         return 0, True
-     
-
-
-    
-    
 
 # Setup
 # Popular lista de turmas
